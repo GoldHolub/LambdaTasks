f07ff490759ae29b1ad939b8ba49c9fecf54410b
Maksym Holubitskyi <golub1718@gmail.com>
2024-04-01 13:42:33 +0300

    Added vacation grouping logic to the app

---
+++ 09_Vacation_grouping/VacationGroupingApp.mjs
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+const FILE_PATH = '09_Vacation_grouping/original-vacation-info.json';
+const NEW_FILE_PATH = '09_Vacation_grouping/new-vacation-info.json';
+
+const readFile = (filePath) => {
+    const vacationFile = fs.readFileSync(filePath);
+    const vacationInfo = JSON.parse(vacationFile);
+    return vacationInfo;
+}
+
+const groupVacationInfo = (vacationInfo) => {
+    let newVacationInfo = new Map();
+    vacationInfo.forEach(element => {
+        const userId = element.user._id;
+        const weekendDates = { startDate: element.startDate, endDate: element.endDate };
+        if (newVacationInfo.has(userId)) {
+            newVacationInfo.get(userId).weekendDates.push(weekendDates);
+        } else {
+            const userName = element.user.name;
+            newVacationInfo.set(userId, { userId, userName, weekendDates: [weekendDates] });
+        }
+    });
+    return Array.from(newVacationInfo.values());
+}
+
+const createNewVacationFile = (newVacationInfo) => {
+    fs.writeFileSync(NEW_FILE_PATH, JSON.stringify(newVacationInfo)); 
+}
+
+const vacationInfo = readFile(FILE_PATH);
+const newVacationInfo = (groupVacationInfo(vacationInfo));
+createNewVacationFile(newVacationInfo);
